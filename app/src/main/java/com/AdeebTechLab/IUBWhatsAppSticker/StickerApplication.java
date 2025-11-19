23185820da509ab61c1db3b7ccd287529ec8395a
Adeeb Technology Lab <135735024+AdeebTechLab@users.noreply.github.com>
2025-11-20 01:47:08 +0500

    update

--- app/src/main/java/com/AdeebTechLab/IUBWhatsAppSticker/StickerApplication.java
+++ app/src/main/java/com/AdeebTechLab/IUBWhatsAppSticker/StickerApplication.java
@@ -11,12 +11,14 @@ package com.AdeebTechLab.IUBWhatsAppSticker;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.soloader.SoLoader;
 
 public class StickerApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        SoLoader.init(this, false);
         Fresco.initialize(this);
     }
 }
